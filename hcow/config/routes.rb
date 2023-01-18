faf84b0193dfff5385279144f51f0d2995a0e23c
bppwd2023-corpuz <cg.corpuz@baypath.net>
2023-01-18 13:41:51 +0000

    Finnally Getting Somewhere

--- hcow/config/routes.rb
+++ hcow/config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
+  get 'static/home'
+  get 'static/about'
+  get 'static/contact'
+  resources :sponsors
+  resources :photos
+  resources :albums
+  resources :events
+  resources :pages
+  resources :sections
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "static#home"
 end
