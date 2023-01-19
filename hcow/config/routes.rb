bbe59c483afd062261c4c491a8e863e846f5d4e2
bppwd2023-corpuz <cg.corpuz@baypath.net>
2023-01-19 14:46:56 +0000

    Calender Added

--- hcow/config/routes.rb
+++ hcow/config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  
+  devise_scope :user do
+    # Redirects signing out users back to sign-in
+    get "users", to: "devise/sessions#new"
+  end
+  devise_for :users
   get 'static/home'
   get 'static/about'
   get 'static/contact'
